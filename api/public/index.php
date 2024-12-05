f1ec929c908d3297d8453144d22fd9ae7d66a8df
Ubuntu <mmppppss@ip-172-26-4-39.ec2.internal>
2024-12-05 20:13:44 +0000

    add cors

--- api/public/index.php
+++ api/public/index.php
@@ -1,15 +1,15 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 session_start();
-if(!isset($_SESSION['token'])){
+/*if(!isset($_SESSION['token'])){
 	echo json_encode([
 		"session" => false,
 		"message" => "Por favor inicia sesion",
 		"error" => 100
 	]);
-	exit();
+	#exit();
 }
-$tokenSession = $_SESSION['token'];
+$tokenSession = $_SESSION['token'];*/
 require_once '../src/router.php';
 new Router();
 ?>
