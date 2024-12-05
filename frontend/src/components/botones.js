69c90ac4a73172f146130a3d42374b4f150885a4
Ubuntu <mmppppss@ip-172-26-4-39.ec2.internal>
2024-12-05 13:43:52 +0000

    feat(botones): agrege funcionalidad a los botones
    Ejemplo para que continuen haciendo

--- frontend/src/components/botones.js
+++ frontend/src/components/botones.js
@@ -2,22 +2,34 @@ import React from "react"
 import "./botones.css"
 
 function defineButtons(permission){
+	var botones={
+		niño:{
+		name: "Ver Niños",
+		route: "/lista"
+	},
+		asistencia:{
+		name: "Ver asistencia",
+		route: "/asistencia"
+	}}
 	if(permission == 0){
-		return ["Ver Niños", "Ver Materias", "Ver Actividades"];
+		return [botones.niño, botones.asistencia]
 	}else if(permission == 1){//maestro
-		return ["Ver Niños", "Ver Materias"];
+		return [botones.asistencia]
 	}
 }
 
 function Boton(nombreBoton){
 	return(
-		<div className="boton" key={nombreBoton}>
-			<input type="submit" value={nombreBoton}/>
+		<div className="boton" key={nombreBoton.name}>
+			<input type="submit" value={nombreBoton.name} onClick={()=>{
+				window.location.href = nombreBoton.route
+			}}/>
 		</div>
 	)
 }
 export default function Botones(props){
 	var a=defineButtons(props.permission);
+	console.log(a)
 	var x=[];
 	for(var i of a){
 		x.push(Boton(i))
